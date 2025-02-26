783a444590d16cded9b55be6b176353e9b532850
trh-thanh30 <tranhuuthanhcp@gmail.com>
2025-02-26 23:48:50 +0700

    created cart router succesfully

--- server/controllers/cart.controllers.js
+++ server/controllers/cart.controllers.js
@@ -1,4 +1,5 @@
 const Cart = require("../models/cart.models");
+const Coupon = require("../models/coupon.models");
 const Product = require("../models/products.models");
 const User = require("../models/user.models");
 
@@ -38,6 +39,67 @@ const addToCart = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+const applyCoupon = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { code } = req.body;
+
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const cart = await Cart.findOne({ user: user.id }).populate(
+      "products.product"
+    );
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+    const codeCoupon = await Coupon.findOne({ code });
+    if (!codeCoupon)
+      return res.status(404).json({ message: "Coupon not found" });
+    let { totalPriceCart } = cart;
+    let priceDiscount = totalPriceCart * (codeCoupon.discount / 100);
+    let totalAfterDiscount =
+      totalPriceCart - parseFloat(priceDiscount.toFixed(3));
+    await Cart.findOneAndUpdate(
+      { user: user.id },
+      { totalAfterDiscount },
+      { new: true }
+    );
+    return res.status(200).json({
+      message: "Apply coupon for the products successfully",
+      totalAfterDiscount,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: error.message });
+  }
+};
+const getCart = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const cart = await Cart.findOne({ user: user.id }).populate(
+      "products.product"
+    );
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+    return res.status(200).json({ cart });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+const emptyCart = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const cart = await Cart.findOneAndReplace({ user: user.id });
+    if (!cart) return res.status(404).json({ message: "Cart not found" });
+    return res.status(200).json({ message: "Cart emptied successfully", cart });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
 module.exports = {
   addToCart,
+  applyCoupon,
+  getCart,
+  emptyCart,
 };
--- server/routers/cart.router.js
+++ server/routers/cart.router.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const verifyToken = require("../middleware/verifyToken");
-const { addToCart } = require("../controllers/cart.controllers");
+const {
+  addToCart,
+  applyCoupon,
+  getCart,
+  emptyCart,
+} = require("../controllers/cart.controllers");
 const router = express.Router();
 // POST
-router.post("/", verifyToken, addToCart)
+router.post("/apply-coupon", verifyToken, applyCoupon);
+router.post("/", verifyToken, addToCart);
+// GET
+router.get("/", verifyToken, getCart);
+// DELETE
+router.delete("/", verifyToken, emptyCart);
 module.exports = router;
